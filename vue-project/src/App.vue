e3a6b8c2eeae9170a77a6a5567502cfe6947a956
fu1fan <fyf.yf@qq.com>
2024-07-03 22:36:11 +0800

    总框架搭建

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,34 @@
 <script setup>
-
+  import Header from './components/Header.vue';
+  import Menu from './components/Menu.vue';
 </script>
 
 <template>
-
+  <div class="common-layout">
+    <el-container class="main-view">
+      <el-header><Header /></el-header>
+      <el-container>
+        <el-aside width="200px"><Menu /></el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style scoped>
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+  .common-layout {
+    width: 100%;
+    height: 100vh;
+  }
+  .Header{
+    text-align: left;
+  }
+  .main-view{
+    height: 100%;
+  }
 
 </style>
---
+++ vue-project/src/components/Menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="false">
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon>
+          <location />
+        </el-icon>
+        <span>概览</span>
+      </template>
+      <el-menu-item-group>
+        <template #title><span>Group One</span></template>
+        <el-menu-item index="1-1">item one</el-menu-item>
+        <el-menu-item index="1-2">item two</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="1-3">item three</el-menu-item>
+      </el-menu-item-group>
+      <el-sub-menu index="1-4">
+        <template #title><span>item four</span></template>
+        <el-menu-item index="1-4-1">item one</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>手动测量</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon>
+        <document />
+      </el-icon>
+      <template #title>历史数据</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon>
+        <setting />
+      </el-icon>
+      <template #title>设置</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup>
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+</script>
+
+<style scoped>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  height: 100%;
+}
+</style>
