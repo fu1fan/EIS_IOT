fee4130585b939bc9b81549a57bb83d3d712a9ed
fu1fan <fyf.yf@qq.com>
2024-07-04 01:57:25 +0800

    flask与守护、更新程序

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,10 +1,31 @@
-import flask
+from flask import Flask, redirect, request
+from threading import Thread
+from werkzeug.serving import make_server
+import time
 
-app = flask.Flask(__name__)
+PORT = 1001
+TOKEN = "123456"
+
+app = Flask(__name__, static_folder='../vue-project/dist', static_url_path='/')
+server = make_server('0.0.0.0', PORT, app)
+
+def _update():
+    open("UPDATE", "w")
+    time.sleep(1)
+    server.shutdown()
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return redirect('/index.html')
+
+@app.route('/update')
+def update():
+    if request.args.get("token") != TOKEN:
+        return "0"
+    Thread(target=_update).start()
+    return "1"
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=1001)
+    thread = Thread(target=server.serve_forever, daemon=True)
+    thread.start()
+    thread.join()
--- flask-project/daemon.py
+++ flask-project/daemon.py
@@ -1,4 +1,23 @@
-import subprocess
+import subprocess, os, time
+
+def update():
+    os.system("git pull")
 
 if __name__ == '__main__':
-    pass
+    try:
+        with open("UPDATE", "r") as f:
+            update()
+        os.remove("UPDATE")
+    except FileNotFoundError:
+        pass
+
+    while(1):
+        subprocess.run(["python", "app.py"])
+        try:
+            with open("UPDATE", "r") as f:
+                update()
+                time.sleep(0.5)
+            # Remove the file "UPDATE"
+            os.remove("UPDATE")
+        except FileNotFoundError:
+            time.sleep(5)
