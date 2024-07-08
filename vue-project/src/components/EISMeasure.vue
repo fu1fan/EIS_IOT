d0774fcb0f575660aff585a6fab6d135180cb3f5
fu1fan <fyf.yf@qq.com>
2024-07-09 02:24:32 +0800

    添加界面

--- vue-project/src/components/EISMeasure.vue
+++ vue-project/src/components/EISMeasure.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import NequistChart from './charts/NequistChart.vue';
 import BatterySelect from './BatterySelect.vue';
 import { ElMessageBox } from 'element-plus';
--- vue-project/src/components/RMeasure.vue
+++ vue-project/src/components/RMeasure.vue
@@ -1,45 +1,105 @@
 <template>
-    <el-row gutter="20">
-      <el-col :span="8">
-        <BatterySelect />
-      </el-col>
-      <el-col :span="16">
-        <el-card shadow="hover">
-          <el-row gutter="20">
-            <el-col :span="16">
-              暂无
-            </el-col>
-            <el-col :span="8">
-              <el-card shadow="never">
-                <template #header>
-                  <div class="card-header">
-                    <span class="card-name">
-                      <span class="align-middle">设备状态</span>
-                    </span>
-                  </div>
-                </template>
-                暂无
-              </el-card>
-            </el-col>
-          </el-row>
-          <template #footer>
-            <div style="display: flex; justify-content: flex-end;">
-              <el-button type="primary">开始测量</el-button>
-            </div>
-          </template>
-        </el-card>
-      </el-col>
-    </el-row>
-  </template>
-  
-  <script setup>
-  import BatterySelect from './BatterySelect.vue';
-  
-  </script>
-  
-  <style scoped>
-  .el-card {
-    height: 100%;
+  <el-row gutter="20">
+    <el-col :span="8">
+      <BatterySelect @update:selectedIndex="updateSelectedIndex" />
+    </el-col>
+    <el-col :span="16">
+      <el-card shadow="hover">
+        <el-text size="large">{{ result }}</el-text>
+        <template #footer>
+          <div style="display: flex; justify-content: flex-end;">
+            <el-button type="primary">开始测量</el-button>
+          </div>
+        </template>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup>
+import BatterySelect from './BatterySelect.vue';
+const selected_cell_id = ref(-1);
+const result = ref('未开始测量');
+
+function updateSelectedIndex(index) {
+  //console.log('updateSelectedIndex', index);
+  selected_cell_id.value = index;
+  console.log(selected_cell_id.value);
+}
+
+function checkResult(task_id) {
+  fetch('/api/c/get_result?id=' + task_id, { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status == "success") {
+        if (data.code == 0) {
+          // data.imags存放着纵坐标
+          // data.reals存放着横坐标
+          // 遍历添加
+          result.value = data.data.zabs + "(" + data.data.zarg + ")";
+        } else {
+          alert('错误代码：' + data.code);
+        }
+        button_disabled.value = false;
+        button_text.value = '开始测量';
+      } else if (data.status == "warning") {
+        if (data.message == "waiting") {
+          button_text.value = '任务排队中';
+        } else if (data.message == "processing") {
+          button_text.value = '任务进行中';
+        }
+        // 如果任务未完成，稍后再次检查
+        setTimeout(() => checkResult(task_id), 500)
+      } else {
+        alert('任务丢失')
+      }
+    })
+    .catch((error) => {
+      alert('获取数据失败');
+      // 可以在这里添加错误处理逻辑，比如重试或者终止
+    });
+}
+
+const start_measure = () => {
+  if (selected_cell_id.value == -1) {
+    console.log(selected_cell_id.value);
+    alert('请选择电池');
   }
-  </style>
-  
+  console.log('start measure');
+  fetch('/api/c/add_task', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "type": "single",
+      "freq": 100,
+      "cell_id": selected_cell_id.value - 1,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status == "success") {
+        let task_id = data.id;
+        clear_data();
+        button_disabled.value = true;
+        button_text.value = '等待...';
+        checkResult(task_id);
+      } else {
+        ElMessageBox.alert('设备离线或正忙', '错误', {
+          confirmButtonText: '确定',
+          type: 'error',
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
+</script>
+
+<style scoped>
+.el-card {
+  height: 100%;
+}
+</style>
