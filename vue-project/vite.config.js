e51eada28c71783e6086720a62ae7d73439318e0
fu1fan <fyf.yf@qq.com>
2024-07-16 17:08:52 +0800

    优化本地调试体验

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -25,4 +25,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://eis.zzzing.cn',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
